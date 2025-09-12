37ac859473ac32c6d6971268a0604e458fec65b2
The woso chart <109988333+wosochart@users.noreply.github.com>
2025-09-12 23:26:45 +0300

    Main files

---
+++ data.js
@@ -0,0 +1,49 @@
+window.graphData = {
+  "nodes": [
+    {
+      "id": "0",
+      "label": "Kathryn Treder",
+      "group": "USA",
+      "team": "Bay Breakers",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/kathryntreder.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(14, 83.58530034544229%, 65.29188035696642%)",
+        "border": "hsl(14, 83.58530034544229%, 65.29188035696642%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "1",
+      "label": "Alev Kelter",
+      "group": "USA",
+      "team": "Bay Breakers",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/alevkelter.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(14, 83.58530034544229%, 65.29188035696642%)",
+        "border": "hsl(14, 83.58530034544229%, 65.29188035696642%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    }
+  ],
+  "edges": [
+    {
+      "from": "0",
+      "to": "1",
+      "label": "Wife",
+      "color": "#9cc2e5",
+      "width": 2
+    }
+  ]
+};
