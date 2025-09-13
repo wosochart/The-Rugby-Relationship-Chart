3796439233f364e3a36e505461f01e2963281428
Helen P <jimmyginas@gmail.com>
2025-09-13 17:56:56 +0300

    some players

--- data.js
+++ data.js
@@ -10,8 +10,8 @@ window.graphData = {
       "image": "img/KathrynTreder.jpg",
       "brokenImage": "img/nonplayer.jpg",
       "color": {
-        "background": "hsl(29, 82.25676312873816%, 66.47543889935632%)",
-        "border": "hsl(29, 82.25676312873816%, 66.47543889935632%)"
+        "background": "hsl(177, 81.55330527151202%, 60.18946396898442%)",
+        "border": "hsl(177, 81.55330527151202%, 60.18946396898442%)"
       },
       "font": {
         "color": "#000",
@@ -28,8 +28,188 @@ window.graphData = {
       "image": "img/AlevKelter.jpg",
       "brokenImage": "img/nonplayer.jpg",
       "color": {
-        "background": "hsl(29, 82.25676312873816%, 66.47543889935632%)",
-        "border": "hsl(29, 82.25676312873816%, 66.47543889935632%)"
+        "background": "hsl(177, 81.55330527151202%, 60.18946396898442%)",
+        "border": "hsl(177, 81.55330527151202%, 60.18946396898442%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "2",
+      "label": "Rosie Galligan",
+      "group": "England",
+      "team": "Saracens",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/RosieGalligan.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "3",
+      "label": "Marlie Packer",
+      "group": "England",
+      "team": "Saracens",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/MarliePacker.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "4",
+      "label": "Sarah Bern",
+      "group": "England",
+      "team": "Bristol Bears",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/SarahBern.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "5",
+      "label": "Mckenzie Carson",
+      "group": "England",
+      "team": "Gloucester-Hartpur",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/MckenzieCarson.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "6",
+      "label": "Hannah Botterman",
+      "group": "England",
+      "team": "Bristol Bears",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/HannahBotterman.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "7",
+      "label": "Holly Aitchison",
+      "group": "England",
+      "team": "Sale Sharks",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/HollyAitchison.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(175, 81.74308516456973%, 72.9434917289636%)",
+        "border": "hsl(175, 81.74308516456973%, 72.9434917289636%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "8",
+      "label": "Katie Whalen",
+      "group": "Ireland",
+      "team": "Leinster",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/KatieWhalen.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(221, 84.48216669493584%, 71.3114456494837%)",
+        "border": "hsl(221, 84.48216669493584%, 71.3114456494837%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "9",
+      "label": "Dannah O’Brien",
+      "group": "Ireland",
+      "team": "Leinster",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/DannahOBrien.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(221, 84.48216669493584%, 71.3114456494837%)",
+        "border": "hsl(221, 84.48216669493584%, 71.3114456494837%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "10",
+      "label": "Michela Sillari",
+      "group": "Italy",
+      "team": "Valsugana",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/MichelaSillari.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(283, 80.40312898919464%, 70.56409115568484%)",
+        "border": "hsl(283, 80.40312898919464%, 70.56409115568484%)"
+      },
+      "font": {
+        "color": "#000",
+        "size": 16
+      }
+    },
+    {
+      "id": "11",
+      "label": "Giordana Duca",
+      "group": "Italy",
+      "team": "Valsugana",
+      "shape": "circularImage",
+      "value": 36,
+      "image": "img/GiordanaDuca.jpg",
+      "brokenImage": "img/nonplayer.jpg",
+      "color": {
+        "background": "hsl(283, 80.40312898919464%, 70.56409115568484%)",
+        "border": "hsl(283, 80.40312898919464%, 70.56409115568484%)"
       },
       "font": {
         "color": "#000",
@@ -43,6 +223,41 @@ window.graphData = {
       "to": "1",
       "label": "Wife",
       "color": "#9cc2e5",
+      "width": 2
+    },
+    {
+      "from": "2",
+      "to": "3",
+      "label": "Wife",
+      "color": "#9cc2e5",
+      "width": 2
+    },
+    {
+      "from": "4",
+      "to": "5",
+      "label": "Wife",
+      "color": "#9cc2e5",
+      "width": 2
+    },
+    {
+      "from": "6",
+      "to": "7",
+      "label": "Wife",
+      "color": "#9cc2e5",
+      "width": 2
+    },
+    {
+      "from": "8",
+      "to": "9",
+      "label": "Gf",
+      "color": "#6aa84f",
+      "width": 2
+    },
+    {
+      "from": "10",
+      "to": "11",
+      "label": "Gf",
+      "color": "#6aa84f",
       "width": 2
     }
   ]
